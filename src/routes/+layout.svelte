bf903eeae69627fb9383ae14abafbfcb76fcbb42
smit patel <smit@smits-MacBook-Pro.local>
2023-08-06 13:47:27 +0530

    vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,10 @@
     import { invalidate, invalidateAll } from '$app/navigation'
 	import { onMount } from 'svelte'
     import { enhance } from "$app/forms";
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
+ 
+    inject({ mode: dev ? 'development' : 'production' });
 
 	export let data
 
